10104465db968db070611967586086504dc15229
Nilesh Agrawal <nilesh_fw12_098@masai.school>
2021-11-19 20:14:05 +0530

    Api_Requested_By Middleware Added

--- Books with middleware/app.js
+++ Books with middleware/app.js
@@ -4,21 +4,32 @@ const books = require('./books.json');
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    return res.send(books);
+const requestedBy = (name) => {
+    return (req, res, next) => {
+      const originalSendFunc = res.send.bind(res);
+      res.send = function (body) {
+        body.api_requested_by = name;
+        return originalSendFunc(body);
+      };
+      next();
+    };
+  };
+
+app.get("/", requestedBy("Nilesh"), (req, res) => {
+    res.send({books});
 });
 
-app.post("/books", (req, res) => {
+app.post("/books", requestedBy("Nilesh"), (req, res) => {
     const newBooks = [...books, req.body];
-    return res.send(newBooks);
+    return res.send({newBooks});
 });
 
-app.get("/books/:id", (req, res) => {
+app.get("/books/:id", requestedBy("Nilesh"), (req, res) => {
     const book = books.filter( b => b.id == req.params.id);
-    return res.send(book);
+    return res.send({book});
 });
 
-app.patch("/books/:id", (req, res) => {
+app.patch("/books/:id", requestedBy("Nilesh"), (req, res) => {
     const newBooks = books.map(book => {
         if(book.id == req.params.id){
             if(req?.body?.title) book.title = req.body.title;
@@ -31,13 +42,13 @@ app.patch("/books/:id", (req, res) => {
         }
         return book;
     });
-    return res.send(newBooks);
+    return res.send({newBooks});
 });
 
-app.delete("/books/:id", (req, res) => {
+app.delete("/books/:id", requestedBy("Nilesh"), (req, res) => {
     const newBooks = books.filter( book => book.id != req.params.id);
 
-    return res.send(newBooks);
+    return res.send({newBooks});
 });
 
 app.listen(3000, () => {
